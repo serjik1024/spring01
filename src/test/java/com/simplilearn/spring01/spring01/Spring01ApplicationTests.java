5599527ffcc2b3eba796df26e6a75fcf55505788
Serj <c-serjik.isagholian@hulu.com>
2020-11-26 15:19:11 -0800

    fancy test added

--- src/test/java/com/simplilearn/spring01/spring01/Spring01ApplicationTests.java
+++ src/test/java/com/simplilearn/spring01/spring01/Spring01ApplicationTests.java
@@ -2,12 +2,20 @@ package com.simplilearn.spring01.spring01;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.*;
 
 @SpringBootTest
 class Spring01ApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void simpleTest(){
+		int a = 1;
+		int b = 2;
+		assertTrue(a + b == 3);
 	}
 
 }
